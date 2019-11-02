2fccd019945fd26a88a0be14f250fc36b34d87c8
Nathan Graule <solarliner@gmail.com>
2019-11-02 17:35:32 +0100

    WIP parser (part n)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::io::{self, Read};
+
 mod parse;
 
-fn main() {
-    println!("{:?}", parse::parse_input("MOV R0, R1"));
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    match parse::parse_input(&input) {
+        Ok(ast) => {
+            println!("{:?}", ast);
+            Ok(())
+        }
+        Err(err) => {
+            eprintln!("{}", err);
+            Ok(())
+        }
+    }
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,42 +1,62 @@
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum Instruction<'a> {
     Label(&'a str),
+    Reg(&'a str, u8),
     RegReg(&'a str, u8, u8),
+    RegPtr(&'a str, u8, u8),
     RegCst(&'a str, u8, u16),
-    RegLabel(&'a str, u8, &'a str)
+    RegLabel(&'a str, u8, &'a str),
 }
 
 peg::parser! {
     grammar asm() for str {
+        use std::usize;
         use super::Instruction;
-        rule number() -> u32
-            = n:$(['0'..='9']+) { n.parse().unwrap() }
-        rule whitespace() = quiet!{[' ' | '\t' | '\n']*}
-        rule instruction<'a>() -> &'a str = i:$(['a'..='z' | 'A'..='Z']) {i}
+        rule ws() = /* quiet!{[' ' | '\t' | '\n']*} */ [' ' | '\t']*
+        rule dec() -> usize
+            = ws() n:$(['0'..='9']+) ws() {n.parse().unwrap()}
+        rule hex() -> usize
+            = ws() "0x" n:$(['0'..='9' | 'a'..='f' | 'A'..='F']) ws() {
+            usize::from_str_radix(n, 16).unwrap()
+        }
+        rule number() -> usize
+            = n:(dec() / hex()) / expected!("number")
+        rule instruction() -> &'input str = ws() i:$(['a'..='z' | 'A'..='Z']+) ws() {i}
         rule constant() -> u16
-            = "#" c:number() {c.into()}
+            = ws() "#" c:number() ws() {c as u16} / expected!("constant")
         rule register() -> u8
-            = "R" r:number() {r.into()}
-        rule label<'a>() -> Instruction<'a>
-            = l:$(['a'..='z' | 'A'..='Z'] (['0'..='9' | 'a'..='z' | 'A'..='Z'])*) {Instruction::Label(l)}
-        rule instruction_rr<'a>() -> Instruction<'a>
-            = i:instruction() whitespace() r1:register() whitespace() "," whitespace() r2:register() {
+            = ws() "R" r:number() ws() {r as u8} / expected!("register")
+        rule label() -> &'input str
+            = ws() l:$(['a'..='z' | 'A'..='Z' | '_'] (['0'..='9' | 'a'..='z' | 'A'..='Z' | '_'])*) ws() {l}
+        rule instruction_r() -> Instruction<'input>
+            = i:instruction() r:register() {Instruction::Reg(i, r)}
+        rule instruction_rr() -> Instruction<'input>
+            = i:instruction() r1:register() "," r2:register() {
             Instruction::RegReg(i, r1, r2)
         }
-        rule instruction_rc<'a>() -> Instruction<'a>
-            = i:instruction() whitespace() r:register() whitespace() "," whitespace() c:constant() {
+        rule instruction_rc() -> Instruction<'input>
+            = i:instruction() r:register() "," c:constant() {
             Instruction::RegCst(i, r, c)
         }
-        rule instruction_rl<'a>() -> Instruction<'a>
-            = i:instruction() whitespace() r:register() whitespace() "," whitespace() l:label() {
+        rule instruction_rl() -> Instruction<'input>
+            = i:instruction() r:register() "," l:label() {
             Instruction::RegLabel(i, r, l)
         }
+        rule instruction_rp() -> Instruction<'input>
+            = i:instruction() r1:register() "," ws() "[" r2:register() "]" ws() {Instruction::RegPtr(i, r1, r2)}
+        rule instruction_label() -> Instruction<'input>
+            = l:label() ":" ws() "\n" {Instruction::Label(l)} / expected!("label")
 
-        pub rule program<'a>() -> Vec<Instruction<'a>>
-            = l:(label() / instruction_rr() / instruction_rc() / instruction_rl() ** whitespace()) {l}
+        rule instruction_program() -> Instruction<'input>
+            = i:(instruction_r() / instruction_rr() / instruction_rc() / instruction_rl() / instruction_rp()) "\n" {i} / expected!("instruction")
+
+        pub rule program() -> Vec<Instruction<'input>>
+            = ws() l:(instruction_program() / instruction_label()) ** ws() ws() {l}
     }
 }
 
-pub fn parse_input(input: &str) -> Result<Vec<Instruction>, peg::error::ParseError<peg::str::LineCol>> {
+pub fn parse_input(
+    input: &str,
+) -> Result<Vec<Instruction>, peg::error::ParseError<peg::str::LineCol>> {
     asm::program(input)
 }
