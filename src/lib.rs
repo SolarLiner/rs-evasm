027c18e952025ff51126a4c9dffdb94c9952ce54
Nathan Graule <solarliner@gmail.com>
2019-11-10 16:41:04 +0100

    Rust format changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use std::io::{self, Write};
+use std::io::Write;
 
 mod generate;
 mod parse;
 
 pub fn assemble<W: Write>(input: &str, buf: &mut W) -> Result<usize, String> {
-    let ast = parse::parse_input(input).map_err(|e| format!("Parsing error: {}", e))?;
-    generate::assemble(ast, buf)
+	let ast = parse::parse_input(input).map_err(|e| format!("Parsing error: {}", e))?;
+	generate::assemble(ast, buf)
 }
